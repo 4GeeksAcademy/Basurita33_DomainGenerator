f0be70dc262d722b361ceed983a1c3034239e65b
Bruno Beceiro <164252484+Basurita33@users.noreply.github.com>
2024-06-03 13:36:17 +0000

    Bruno first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "my", "some"];
+
+  let adjectives = ["cool", "anarchist", "awesome", "diabolic"];
+
+  let nouns = ["hacker", "code", "geeks", "tech"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        domainNames.push(`${pronoun}${adjective}${noun}.com`);
+      }
+    }
+  }
+  let domainList = domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join("");
+  document.querySelector("ul").innerHTML = domainList;
 };
